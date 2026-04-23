f156899f4d9afab9b0ceb2e540a9d8edab9e45b0
greatbabyred <sjhardy013@gmail.com>
2026-04-22 23:06:02 -0600

    Added a report at the end of each round which documents the user's resources spent and gained

--- Scripts/GameRoot.cs
+++ Scripts/GameRoot.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using Godot.Collections;
 using System.Collections;
+using Collections = System.Collections.Generic;
 
 public partial class GameRoot : Node2D
 {
@@ -15,7 +16,8 @@ public partial class GameRoot : Node2D
     private RunState runState;
     private Queue lootQueue = new Queue();
     private int nextUid = 1;
-    private bool choiceScreenActive = false;
+    private bool isScreenBusy = false;
+    [Export] private PackedScene upkeepReportScene;
     public override void _Ready()
     {		
         database = ResourceLoader.Load<GameDatabase>("res://Resources/Definitions/GameDatabase.tres");
@@ -62,7 +64,8 @@ public partial class GameRoot : Node2D
                 placementController.BeginPlacement(InstantiatePlaceable(def));
             }
             GD.Print($"Player picked: {chosenLoot.CoreAttributes.DisplayName}");
-            DeactivateChoiceScreen();
+            MarkScreenAsFree();
+            choiceScreen.Hide();
         };        
         signalBus.PlaceablePlaced += (GridPlaceable placeable) =>
         {
@@ -125,53 +128,129 @@ public partial class GameRoot : Node2D
     }
     public override void _Process(double delta)
     {
-        if (!choiceScreenActive && lootQueue.Count > 0)
+        if (!isScreenBusy && lootQueue.Count > 0)
         {
             PendingLoot pendingLoot = (PendingLoot)lootQueue.Dequeue();
-            ActivateChoiceScreen();
+            MarkScreenAsBusy();
+            choiceScreen.Show();
             choiceScreen.GenerateCards(database, pendingLoot.num, pendingLoot.lootType, runState.Wave);
         }
     }
-    private void ActivateChoiceScreen()
+    private void MarkScreenAsBusy()
     {
-        choiceScreenActive = true;
+        isScreenBusy = true;
         camera.DisableControls();
         hud.Hide();
-        choiceScreen.Show();
     }
-    private void DeactivateChoiceScreen()
+    private void MarkScreenAsFree()
     {
-        choiceScreenActive = false;
-        choiceScreen.Hide();
+        isScreenBusy = false;
         hud.Show();
         camera.EnableControls();
     }
     private void ResolveUpkeep()
     {
+
         for(int i = 0; i < runState.pendingConstruction; i++)
         {
             lootQueue.Enqueue(new PendingLoot(3, LootType.Building));
         }
-         runState.pendingConstruction = 0;
+        runState.pendingConstruction = 0;
+        Collections.Queue<int> foodChanges = new Collections.Queue<int>();
+        Collections.Queue<string> foodChangeSources = new Collections.Queue<string>();
+        Collections.Queue<int> woodChanges = new Collections.Queue<int>();
+        Collections.Queue<string> woodChangeSources = new Collections.Queue<string>();
+        Dictionary<string, int[]> foodProduction = new Dictionary<string, int[]>();
+        Dictionary<string, int[]> woodProduction = new Dictionary<string, int[]>();
+        int totalBuildingFoodUpkeep = 0;
+        int totalBuildingWoodUpkeep = 0;
+        int totalUnitFoodUpkeep = 0;
+        bool decreasedFoodEfficiency = false;
         foreach(GridPlaceable placeable in runState.ActivePlaceables)
         {
             if(placeable.def is BuildingDefinition buildingDef)
             {
+                //Add Resources
                 runState.AddResources(buildingDef.ProductionFoodPerRound, buildingDef.ProductionWoodPerRound);
+                if(buildingDef.ProductionFoodPerRound != 0)
+                {
+                    if(foodProduction.ContainsKey(buildingDef.CoreAttributes.DisplayName))
+                    {
+                        GD.Print("Multiple " + buildingDef.CoreAttributes.DisplayName + " detected, stacking production in upkeep report.");
+                        foodProduction[buildingDef.CoreAttributes.DisplayName] = 
+                        [
+                            foodProduction[buildingDef.CoreAttributes.DisplayName][0] + buildingDef.ProductionFoodPerRound, 
+                            foodProduction[buildingDef.CoreAttributes.DisplayName][1] + 1
+                        ];
+                        GD.Print("Total " + buildingDef.CoreAttributes.DisplayName + " production: " + foodProduction[buildingDef.CoreAttributes.DisplayName][0] + " from " + foodProduction[buildingDef.CoreAttributes.DisplayName][1] + " buildings.");
+                    } else
+                    {
+                        foodProduction[buildingDef.CoreAttributes.DisplayName] = [buildingDef.ProductionFoodPerRound, 1];
+                    }
+                }
+                if(buildingDef.ProductionWoodPerRound != 0)
+                {
+                    if(woodProduction.ContainsKey(buildingDef.CoreAttributes.DisplayName))
+                    {
+                        woodProduction[buildingDef.CoreAttributes.DisplayName][0] += buildingDef.ProductionWoodPerRound;
+                        woodProduction[buildingDef.CoreAttributes.DisplayName][1] ++;
+                    } else
+                    {
+                        woodProduction[buildingDef.CoreAttributes.DisplayName] = [buildingDef.ProductionWoodPerRound, 1];
+                    } 
+                }
+
+                //Spend Resources
                 if(runState.TrySpendResources(0, buildingDef.UpkeepWoodPerRound))
                 {
                     runState.ForceSpendResources(placeable.def.UpkeepFoodPerRound, 0);
+                    totalBuildingFoodUpkeep += placeable.def.UpkeepFoodPerRound;
+                    totalBuildingWoodUpkeep += buildingDef.UpkeepWoodPerRound;
                 }
                 else
                 {
+                    decreasedFoodEfficiency = true;
                     runState.ForceSpendResources(placeable.def.UpkeepFoodPerRound * 2 + buildingDef.UpkeepWoodPerRound, 0);
+                    totalBuildingFoodUpkeep += placeable.def.UpkeepFoodPerRound * 2 + buildingDef.UpkeepWoodPerRound;
                 }
             }
             if(placeable.def is UnitDefinition)
             {
                 runState.ForceSpendResources(placeable.def.UpkeepFoodPerRound, 0);
+                totalUnitFoodUpkeep += placeable.def.UpkeepFoodPerRound;
             }
         }
+        foreach(string entry in foodProduction.Keys)
+        {
+            if(foodProduction[entry][1] > 1)
+            {
+                foodChangeSources.Enqueue($"{foodProduction[entry][1]}× {entry}");
+            } else {
+                foodChangeSources.Enqueue(entry);
+            }
+            foodChanges.Enqueue(foodProduction[entry][0]);
+        }
+        foodChangeSources.Enqueue("Building Upkeep");
+        foodChanges.Enqueue(-totalBuildingFoodUpkeep);
+        foodChangeSources.Enqueue("Unit Upkeep");
+        foodChanges.Enqueue(-totalUnitFoodUpkeep);
+        foreach(string entry in woodProduction.Keys)
+        {
+            if(woodProduction[entry][1] > 1)
+            {
+                woodChangeSources.Enqueue($"{woodProduction[entry][1]}x {entry}");
+            } else {
+                woodChangeSources.Enqueue(entry);
+            }
+            woodChanges.Enqueue(woodProduction[entry][0]);
+        }
+        woodChangeSources.Enqueue("Building Upkeep");
+        woodChanges.Enqueue(-totalBuildingWoodUpkeep);
+        UpkeepReport upkeepReport = upkeepReportScene.Instantiate<UpkeepReport>();
+        upkeepReport.Initialize(foodChangeSources, foodChanges, woodChangeSources, woodChanges, runState.Food, runState.Wood, decreasedFoodEfficiency);
+        AddChild(upkeepReport);
+        MarkScreenAsBusy();
+        upkeepReport.TreeExited += MarkScreenAsFree;
         lootQueue.Enqueue(new PendingLoot(3));
     }
     private GridPlaceable InstantiatePlaceable(PlaceableDefinition def)
---
+++ Scripts/UpkeepReport.cs
@@ -0,0 +1,87 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+
+public partial class UpkeepReport : CanvasLayer
+{
+    [Export] public NodePath buttonPath;
+    [Export] public NodePath newFoodPath;
+    [Export] public NodePath newWoodPath;
+    private VBoxContainer contentBox;
+    public void Initialize(Queue<string> foodChangeSources, Queue<int> foodChanges, Queue<string> woodChangeSources, Queue<int> woodChanges, int newFood, int newWood, bool insufficientWood)
+    {
+        contentBox = GetNode<VBoxContainer>("ContentBox");
+        foreach(Node child in contentBox.GetChildren())
+        {
+            if (child is HBoxContainer && child.Name != "Totals")
+            {
+                child.QueueFree();
+            }
+        }
+        GetNode<Button>(buttonPath).Pressed += () => QueueFree();
+        int totalFoodChange = 0;
+        while(foodChangeSources.Count > 0)
+        {
+            string source = foodChangeSources.Dequeue();
+            int foodChange = foodChanges.Dequeue();
+            totalFoodChange += foodChange;
+            AddEntry(source, foodChange, "Food");
+        }
+        AddBreak();
+        AddEntry("Total Food Upkeep", totalFoodChange, "Food");
+        AddBreak();
+        int totalWoodChange = 0;
+        while(woodChangeSources.Count > 0){
+            string source = woodChangeSources.Dequeue();
+            int woodChange = woodChanges.Dequeue();
+            totalWoodChange += woodChange;
+            AddEntry(source, woodChange, "Wood");
+        }
+        AddBreak();
+        AddEntry("Total Wood Upkeep", totalWoodChange, "Wood");
+        if(insufficientWood)
+        {
+            Label warningLabel = new Label();
+            warningLabel.Text = "! Residents don't have enough wood to cook their food. Food consumption efficiency greatly decreased.";
+            warningLabel.AutowrapMode = TextServer.AutowrapMode.WordSmart;
+            warningLabel.AddThemeColorOverride("font_color", new Color(0.8f, 0.5f, 0.2f));
+            warningLabel.AddThemeFontSizeOverride("font_size", 12);
+            contentBox.AddChild(warningLabel);
+            contentBox.MoveChild(warningLabel, contentBox.GetChildCount() - 4);
+        }
+        GetNode<Label>(newFoodPath).Text = $"Food: {newFood}";
+        GetNode<Label>(newWoodPath).Text = $"Wood: {newWood}";
+    }
+    public void AddEntry(string source, int number, string resourceType)
+    {
+        HBoxContainer entry = new HBoxContainer();
+        Label sourceLabel = new Label();
+        sourceLabel.Text = source;
+        Label numberLabel = new Label();
+        if(number >=0)
+        {
+            numberLabel.AddThemeColorOverride("font_color", new Color(0.2f, 0.8f, 0.2f));
+            numberLabel.Text = "+" + number.ToString();
+        } else if(number < 0)
+        {
+            numberLabel.AddThemeColorOverride("font_color", new Color(0.8f, 0.2f, 0.2f));
+            numberLabel.Text = number.ToString();
+        }
+        numberLabel.HorizontalAlignment = HorizontalAlignment.Right;
+        numberLabel.SizeFlagsHorizontal = Control.SizeFlags.ExpandFill;
+        Label typeLabel = new Label();
+        typeLabel.Text = resourceType;
+        entry.AddChild(sourceLabel);
+        entry.AddChild(numberLabel);
+        entry.AddChild(typeLabel);
+        contentBox.AddChild(entry);
+        contentBox.MoveChild(entry, contentBox.GetChildCount() - 4);
+    }
+    public void AddBreak()
+    {
+        HSeparator separator = new HSeparator();
+        contentBox.AddChild(separator);
+        contentBox.MoveChild(separator, contentBox.GetChildCount() - 4);
+    }
+}
