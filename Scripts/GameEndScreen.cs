b37f65e0354edc5e49799f631c0fd9cfd7f72875
Spencer Hardy <sjhardy013@gmail.com>
2026-04-26 15:29:01 -0600

    Added game loss screen

---
+++ Scripts/GameEndScreen.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class GameEndScreen : CanvasLayer
+{
+	[Export] public NodePath largeMessage;
+	[Export] public NodePath roundsMessage;
+	[Export] public NodePath completionMessage;
+	[Export] public NodePath crystalMessage;
+	[Export] public NodePath button;
+	public void Initialize(bool win, int[] roundsCompleted, int crystals){
+		if (win)
+		{
+			GetNode<Label>(largeMessage).Text = "You win!";
+			GetNode<Label>(completionMessage).Text = "Game completed";
+		} else
+		{
+			GetNode<Label>(largeMessage).Text = "You lose!";
+			GetNode<Label>(completionMessage).Text = "Game not completed";
+		}
+		GetNode<Label>(roundsMessage).Text = $"Rounds completed: {roundsCompleted[0]}/{roundsCompleted[1]}";
+		GetNode<Label>(crystalMessage).Text = $"+{crystals} Crystals earned";
+	}
+}
